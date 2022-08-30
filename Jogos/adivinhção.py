830bb7a99bc212d6763753ca2929afa004842ccf
Luiz Chiari <luizfernandochiari@gmail.com>
2022-08-30 15:01:05 -0300

    Refatora Jogo de Adivinhação inserindo vaiaveis

--- Jogos/adivinhção.py
+++ Jogos/adivinhção.py
@@ -10,9 +10,15 @@ print("Você digitou", chute_str)
 
 chute = int(chute_str)
 
-if (numero_secreto == chute):
+acertou = chute == numero_secreto
+maior   = chute > numero_secreto
+menor   = chute < numero_secreto
+
+if (acertou):
     print("Você acertou!")
 else:
-    print("Você errou!")
-
+    if(maior):
+        print("Você errou! O seu chute foi maior que o numero secreto")
+    if(menor):
+        print("Você errou! O seu chute foi menor que o numero secreto")
 print("Fim de Jogo")
